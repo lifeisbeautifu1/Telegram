20079ab548de8be74da5440791beb37529deaadd
Potato <poltoradnevone@gmail.com>
2022-09-12 16:03:36 +0400

    bug fixes

--- client/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hooks_1 = require("../app/hooks");
-const react_router_dom_1 = require("react-router-dom");
-const ProtectedRoute = ({ children }) => {
-    const { user } = (0, hooks_1.useAppSelector)((state) => state.auth);
-    return <>{user ? children : <react_router_dom_1.Navigate to="/login"/>}</>;
-};
-exports.default = ProtectedRoute;
--- src/routes/message.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const message_1 = require("../controllers/message");
-const router = (0, express_1.Router)();
-router.post('/', message_1.sendMessage);
-router.get('/:chatId', message_1.getAllMessages);
-exports.default = router;
