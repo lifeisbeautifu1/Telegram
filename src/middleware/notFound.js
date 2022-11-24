67254a5c2c25a53eb72955f5871fe766fa381e58
Potato <poltoradnevone@gmail.com>
2022-11-24 22:59:33 +0400

    fixes

--- src/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_codes_1 = require("http-status-codes");
-exports.default = async (req, res) => {
-    res
-        .status(http_status_codes_1.StatusCodes.NOT_FOUND)
-        .json({ message: 'The page you are looking for does not exist.' });
-};
