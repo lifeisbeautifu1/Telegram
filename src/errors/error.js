5dfbddaef629c2819d22a59f724e03375354cd77
Potato <poltoradnevone@gmail.com>
2022-11-25 09:53:09 +0400

    fix: removed js files & trigger

--- src/errors/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CustomApiError extends Error {
-    constructor(message) {
-        super(message);
-        this.statusCode = 500;
-    }
-}
-exports.default = CustomApiError;
